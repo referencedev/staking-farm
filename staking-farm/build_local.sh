5c459da81f461c01fce28fb8a0183c3ff75b3d13
marioiordanov <mio@shardlabs.io>
2022-05-23 23:51:21 +0300

    build wasm file

--- staking-farm/build_local.sh
+++ staking-farm/build_local.sh
@@ -3,4 +3,4 @@ set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo +stable build --target wasm32-unknown-unknown --release
 cd ..
-cp target/wasm32-unknown-unknown/release/staking_farm.wasm ./res/staking_farm_local.wasm
+cp target/wasm32-unknown-unknown/release/staking_farm.wasm ./res/staking_farm_release.wasm
